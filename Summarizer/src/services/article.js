876022f3c594ddc516fe7717d084d52db8699e09
Hafsajillani <jillanihafsa@gmail.com>
2025-07-15 23:47:56 +0500

    gemini api key updated

--- Summarizer/src/services/article.js
+++ Summarizer/src/services/article.js
@@ -1,13 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const rapidApiKey = "4815dfee9amshf5e3d4a11588f43p1c3c0bjsncd52e3d0dc4f";
+// const rapidApiKey = "4815dfee9amshf5e3d4a11588f43p1c3c0bjsncd52e3d0dc4f"; // RapidAPI key (commented out)
+const geminiApiKey = "AIzaSyB0OlP_G1JsBRcPqML0_cDMNcAKo7AB_4A"; // Gemini API key (for testing)
 
 export const articleApi = createApi({
   reducerPath: 'articleApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', rapidApiKey);
+      headers.set('X-RapidAPI-Key', geminiApiKey);
       headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
 
       return headers;
